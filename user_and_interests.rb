0e6e44cff3dc48083d2a45952a8402146b19e55b
Fernanda Franco <francodias.fernanda@gmail.com>
2017-05-03 20:24:40 -0400

    all implemented

--- user_and_interests.rb
+++ user_and_interests.rb
@@ -3,8 +3,29 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
-get "/" do
-  @info = Psych.load_file("users.yaml")
+helpers do
+  def count_interests
+    interests =  []
+    @info.each {|_, infos| interests << infos[:interests]}
+    interests.flatten.size
+  end
+end
 
+before do
+  @info = Psych.load_file("users.yaml")
+  @users = @info.keys
+end
+
+get "/" do
+  redirect "/users"
+end
+
+get "/users" do
   erb :users
+end
+
+get "/:name" do
+  @name = params[:name]
+  @others = @users.reject { |user| user == @name.to_sym}
+  erb :user_page
 end
